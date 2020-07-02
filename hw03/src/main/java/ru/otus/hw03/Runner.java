fc1e18aec02a516fc1dd2adf9125b4f7a4c77e7a
Балин Константин Витальевич <konstantin.balin@ligastavok.ru>
2020-07-02 18:31:32 +0300

    hw-03 change bundle configuration

--- hw03/src/main/java/ru/otus/hw03/Runner.java
+++ hw03/src/main/java/ru/otus/hw03/Runner.java
@@ -5,16 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import ru.otus.hw03.controller.QAController;
 
-
-
 @SpringBootApplication
-
 public class Runner {
 
-	public static void main(String[] args) throws Exception {
-		ApplicationContext appContext = SpringApplication.run(Runner.class, args);
-		QAController controller = appContext.getBean(QAController.class);
-		controller.startTest();
-	}
+    public static void main(String[] args) throws Exception {
+        ApplicationContext appContext = SpringApplication.run(Runner.class, args);
+        QAController controller = appContext.getBean(QAController.class);
+        controller.startTest();
+    }
 
 }
